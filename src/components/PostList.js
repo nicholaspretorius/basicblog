dd9f1c70ec7d776ea6f885a7324e7f69a673a611
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-08-30 14:31:18 +0200

    Initial setup.

---
+++ src/components/PostList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class PostList extends React.Component {
+  renderPost = () => {
+    const { posts } = this.props;
+
+    if (!posts) {
+      return <>Loading...</>;
+    }
+
+    return posts.map(post => {
+      return (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+        </div>
+      );
+    });
+  };
+
+  render() {
+    const postList = this.renderPost();
+    return <div>{postList}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps)(PostList);
